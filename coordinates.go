6d1c95e1374fa1662d1c4f55fca22fdd77b76aec
Wenky <771623548@qq.com>
2021-12-03 20:24:37 -0500

    Commit

--- coordinates.go
+++ coordinates.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+)
+
+func timeObject(timestamp int32) [7]int32 {
+	year := (1970 + (timestamp / 31556952))
+	month := ((timestamp % 31556952) / 2629746)
+	day := (((timestamp % 31556952) % 2629746) / 86400)
+	hour := ((((timestamp % 31556952) % 2629746) % 86400) / 3600)
+	minute := (((((timestamp % 31556952) % 2629746) % 86400) % 3600) / 60)
+	second := (((((timestamp % 31556952) % 2629746) % 86400) % 3600) % 60)
+
+	return [7]int32{timestamp, year, month, day, hour, minute, second}
+}
+
+func coordinate(ra, dec float32) [2]float32 {
+	return [2]float32{ra, dec}
+}
+
+func AltAz(coordObj [2]float32, obsTime [7]int32, location [3]float32) {
+	fmt.Println(coordObj)
+	fmt.Println(obsTime)
+	fmt.Println(location)
+
+}
--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+var duration = [2]uint32{1638568832, 1638578832}
+var telescope = [3]float32{31, 121.9, 10} //[Lat., Lon., Hei.] TM Tele.
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("TEST")
+	timeObj := time(1638568832)
+	coordObj := coordinate(0.1, -0.1)
+	AltAz(coordObj, timeObj, telescope)
 }
