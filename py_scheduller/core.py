16b6e24e59c7ff1015bdd64d4d4424c5e93c09de
Wenky <771623548@qq.com>
2022-03-27 17:46:15 -0400

    Update core.py

--- py_scheduller/core.py
+++ py_scheduller/core.py
@@ -1,20 +1,11 @@
-'''
-Author: your name
-Date: 2022-03-17 21:20:04
-LastEditTime: 2022-03-17 23:36:34
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /AstroSchedullerGo/Python/core.py
-'''
-
 # go build -buildmode=c-shared -o _scheduller.so *.go
 
 import os
 import json
 import requests
 import ctypes
-from utilities import utilities
-from config import config
+from .utilities import utilities
+from .config import config
 
 class core():
     def __init__(self):
