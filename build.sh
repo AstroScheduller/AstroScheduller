19b60888069d94303345e4c23bf040caa6df6416
Wenky <771623548@qq.com>
2022-03-29 20:07:49 -0400

    Built for Multi Platforms

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-GOOS=linux
-go build -o ./releases_latest/scheduller_linux_amd64 ./*.go
-go build -buildmode=c-shared -o ./releases_latest/_scheduller_linux_amd64.so ./*.go
+# brew install FiloSottile/musl-cross/musl-cross
+# brew install mingw-w64
+
+GOOS=darwin go build -o ./releases_latest/scheduller_darwin_amd64 ./*.go
+GOOS=darwin go build -buildmode=c-shared -o ./releases_latest/_scheduller_darwin_amd64.so ./*.go
+echo "Build success - Darwin"
+md5 ./releases_latest/scheduller_darwin_amd64
+
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -o ./releases_latest/scheduller_linux_amd64 ./*.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -buildmode=c-shared -o ./releases_latest/_scheduller_linux_amd64.so ./*.go
 echo "Build success - Linux"
 md5 ./releases_latest/scheduller_linux_amd64
 
-GOOS=windows
-go build -o ./releases_latest/scheduller_windows_amd64.exe ./*.go
-go build -buildmode=c-shared -o ./releases_latest/_scheduller_windows_amd64.dll ./*.go
+CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -o ./releases_latest/scheduller_windows_amd64.exe ./*.go
+CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -buildmode=c-shared -o ./releases_latest/_scheduller_windows_amd64.dll ./*.go
 echo "Build success - Windows"
 md5 ./releases_latest/scheduller_windows_amd64.exe
-
-GOOS=darwin
-go build -o ./releases_latest/scheduller_darwin_amd64 ./*.go
-go build -buildmode=c-shared -o ./releases_latest/_scheduller_darwin_amd64.so ./*.go
-echo "Build success - Darwin"
-md5 ./releases_latest/scheduller_darwin_amd64
--- releases_latest/_scheduller_windows_amd64.h
+++ releases_latest/_scheduller_windows_amd64.h
@@ -38,8 +38,8 @@ typedef int GoInt32;
 typedef unsigned int GoUint32;
 typedef long long GoInt64;
 typedef unsigned long long GoUint64;
-typedef GoInt64 GoInt;
-typedef GoUint64 GoUint;
+typedef GoInt32 GoInt;
+typedef GoUint32 GoUint;
 typedef __SIZE_TYPE__ GoUintptr;
 typedef float GoFloat32;
 typedef double GoFloat64;
@@ -50,7 +50,7 @@ typedef double _Complex GoComplex128;
   static assertion to make sure the file is being used on architecture
   at least with matching size of GoInt.
 */
-typedef char _check_for_64_bit_pointer_matching_GoInt[sizeof(void*)==64/8 ? 1:-1];
+typedef char _check_for_32_bit_pointer_matching_GoInt[sizeof(void*)==32/8 ? 1:-1];
 
 #ifndef GO_CGO_GOSTRING_TYPEDEF
 typedef _GoString_ GoString;
@@ -70,8 +70,8 @@ extern "C" {
 
 
 //import "strings"
-extern void py_schedule(char* xmlPathImport, char* xmlPathExport);
-extern char* py_AltAz(char* raStr, char* decStr, char* timestampStr, char* teleLatStr, char* teleLonStr, char* teleAltStr);
+extern __declspec(dllexport) void py_schedule(char* xmlPathImport, char* xmlPathExport);
+extern __declspec(dllexport) char* py_AltAz(char* raStr, char* decStr, char* timestampStr, char* teleLatStr, char* teleLonStr, char* teleAltStr);
 
 #ifdef __cplusplus
 }
