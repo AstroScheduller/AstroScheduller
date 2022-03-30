00ec3438c100c24ecc8ca278028be10eb45fa3cd
Wenky <771623548@qq.com>
2022-03-30 19:37:45 -0400

    update builds

--- build.sh
+++ build.sh
@@ -5,24 +5,24 @@ echo "Building..."
 echo "=========================="
 
 echo "Building for Darwin (MacOS/amd64)..."
-CGO_ENABLED=1 GOOS=darwin go build -o ./releases_latest/scheduller_darwin_amd64 ./*.go
-CGO_ENABLED=1 GOOS=darwin go build -buildmode=c-shared -o ./releases_latest/_scheduller_darwin_amd64.so ./*.go
+CGO_ENABLED=1 env GOOS=darwin go build -o ./releases_latest/scheduller_darwin_amd64 ./*.go
+CGO_ENABLED=1 env GOOS=darwin go build -buildmode=c-shared -o ./releases_latest/_scheduller_darwin_amd64.so ./*.go
 md5 ./releases_latest/scheduller_darwin_amd64
 md5 ./releases_latest/_scheduller_darwin_amd64.so
 
 echo "=========================="
 
 echo "Building for Linux (Linux/amd64)..."
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -o ./releases_latest/scheduller_linux_amd64 ./*.go
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -buildmode=c-shared -o ./releases_latest/_scheduller_linux_amd64.so ./*.go
+CGO_ENABLED=1 env GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -o ./releases_latest/scheduller_linux_amd64 ./*.go
+CGO_ENABLED=1 env GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++ go build -buildmode=c-shared -o ./releases_latest/_scheduller_linux_amd64.so ./*.go
 md5 ./releases_latest/scheduller_linux_amd64
 md5 ./releases_latest/_scheduller_linux_amd64.so
 
 echo "=========================="
 
 echo "Building for Windows (Windows/amd64)..."
-CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -o ./releases_latest/scheduller_windows_amd64.exe ./*.go
-CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -buildmode=c-shared -o ./releases_latest/_scheduller_windows_amd64.dll ./*.go
+CGO_ENABLED=1 env GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -o ./releases_latest/scheduller_windows_amd64.exe ./*.go
+CGO_ENABLED=1 env GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -buildmode=c-shared -o ./releases_latest/_scheduller_windows_amd64.dll ./*.go
 md5 ./releases_latest/scheduller_windows_amd64.exe
 md5 ./releases_latest/_scheduller_windows_amd64.dll
 
