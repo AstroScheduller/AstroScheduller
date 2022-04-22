100c3a793f514f020510d8f0909affc86e861ee3
Wenky <771623548@qq.com>
2022-04-22 17:00:03 -0400

    brew command not found

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [ "$(uname)" = "Darwin" ]; then
     # Check if brew is installed
     if [ ! -f /usr/local/bin/brew ]; then
         echo "Installing Homebrew..."
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 fi
 
